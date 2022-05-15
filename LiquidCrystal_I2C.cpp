36db92615ebcc71c8ec72a10671e26462a7e1b71
manasdeore <manasdeore@gmail.com>
2022-05-15 15:18:59 +0530

    Update LiquidCrystal_I2C.cpp

--- LiquidCrystal_I2C.cpp
+++ LiquidCrystal_I2C.cpp
@@ -63,7 +63,7 @@ void LiquidCrystal_I2C::init(){
 
 void LiquidCrystal_I2C::init_priv()
 {
-	Wire.begin();
+	Wire.begin(32,33);
 	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
 	begin(_cols, _rows);  
 }
